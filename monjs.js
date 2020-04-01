8c6a0df38dade29209dc7b19c5b2b552ee904ffd
Kirua07 <Kirua07@users.noreply.github.com>
2020-04-01 12:17:06 +0200

    casi fini

--- monjs.js
+++ monjs.js
@@ -1,3 +1,20 @@
-$ ( document ) . prêt ( fonction ( ) {
+$(document).ready(function(){
+    var width = $('#container').width();
+    console.log(width);
+    $('#container div').height(width);
+
+    $('#table')
+        var $ongletItems = $(".onglets li") ;
     
-} )
+		
+		$ongletItems.click(function(){
+			
+			$ongletItems.removeClass("active") ;
+			
+			$(this).addClass("active") ;
+			
+			return false ;
+			
+        });
+
+});
